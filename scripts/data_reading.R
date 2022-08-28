fd5ad52c4321a234cb993517cecc9a0df83e62ea
León Hernández Robles <leonhdzrobles@gmail.com>
2022-08-27 19:39:42 -0500

    Updated scripts. Deleted unneeded files.

--- scripts/data_reading.R
+++ scripts/data_reading.R
@@ -10,11 +10,5 @@ chile_raw = search_tweets(q="boric OR chile OR constitucion OR apruebo OR rechaz
                       n = 1000
                       )
 
-tidy_chile = chile_raw %>%
-  select(id_str,full_text) %>%
-  unnest_tokens("word", full_text)
-
-
 write_as_csv(chiledf, "products/csv/chile_tweets_raw.csv", prepend_ids = TRUE, 
              na = "", fileEncoding = "UTF-8")
-write.csv(tidy_chile,"products/csv/tidy_chile.csv")
