d9c42030db18fd56a4280d90bf965ffd40aaedbb
León Hernández Robles <leonhdzrobles@gmail.com>
2022-09-13 10:53:20 -0500

    Extractor de texto incluye todos los archivos del folder papers.

--- scripts/setup_analysis.py
+++ scripts/setup_analysis.py
@@ -4,3 +4,4 @@ import os
 dir_py = os.path.dirname(os.path.realpath('__file__'))
 dir_proj = os.path.dirname(os.path.realpath(dir_py))
 dir_papers = os.path.join(dir_proj,'papers')
+dir_products = os.path.join(dir_proj,'products')
