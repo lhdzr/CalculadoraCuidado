b19bd0b47a92c86408f1fbd165d900b869fb9a77
León Hernández Robles <leonhdzrobles@gmail.com>
2022-11-04 11:51:02 -0600

    Generacion Modelos regresa dataframe describiendo modelos y sus resultados

--- Generacion Modelos.R
+++ Generacion Modelos.R
@@ -4,8 +4,6 @@ enut=read.csv("products/csv/enut.csv")
 enut = enut %>%
   mutate(X=NULL)
 
-
-par_var_y = "ENT"
 par_var_x = c("SEXO","EDAD","ESC")
 ncols_vars_x = length(par_var_x)
 l=rep(list(0:1),ncols_vars_x)
@@ -14,16 +12,22 @@ grid_vars_x=expand.grid(l)[-1,]
 names(grid_vars_x)
 grid_vars_x = grid_vars_x %>%
   rename(SEXO=Var1,EDAD=Var2,ESC=Var3)
+rownames(grid_vars_x) <- 1:nrow(grid_vars_x)
 
+### Generar dataframe para describir los modelos generados
+modelos=data.frame(variables=character(0),R2=numeric(0),R2_ajustada=numeric(0))
+
+### Hacer loop por cada fila y columna, e incluir en el modelo solamente aquellas variables con 1 en esa fila
 for (row in 1:nrow(grid_vars_x)) {
   output = ""
   for (column in 1:ncol(grid_vars_x)) {
     if(grid_vars_x[row,column]==1){
       output=paste(output,names(grid_vars_x)[column],sep = "+")}}
   output=str_remove(output,"^\\+")
-  print(paste("Las variables del modelo son",output))
+  ### Incluir las variables usadas en cada modelo, así como sus R2 normal y ajustada.
+  modelos[row,"variables"] = output
   output=paste("SAL_SEM ~",output)
   modelo = lm(eval(parse(text = output)),data = enut)
-  print(paste("La R cuadrada es: ",summary(modelo)$r.squared,sep = ""))
-  print(paste("La R cuadrada ajustada es:",summary(modelo)$adj.r.squared,sep = ""))
+  modelos[row,"R2"] = summary(modelo)$r.squared
+  modelos[row,"R2_ajustada"] = summary(modelo)$r.squared
 }
